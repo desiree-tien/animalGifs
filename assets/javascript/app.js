54fd70f64e711c14000ded2d7af10c7bd18c6949
Desiree Tien <desireetien@Aphesis.local>
2017-11-21 11:19:23 -0600

    Solved gif problem, updatting format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var GIFsAppear = $(".showGIF").on("click", function() {
   var animalName = $(this).attr("data-animal");
 
   // Constructing a queryURL using the animal name
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=4";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=5";
 
   // Performing an AJAX request with the queryURL
   $.ajax({
@@ -25,42 +25,32 @@ var GIFsAppear = $(".showGIF").on("click", function() {
 
         // Creating and storing a div tag
         var animalDiv = $("<div>");
-        animalDiv.addClass("GIFsection");
-        
-        // Creating a paragraph tag with the result item's rating
-        var p = $("<p>").text("Rating: " + results[i].rating);
-        p.addClass("padding");
 
-        // Creating and storing an image tag
-        var animalImage = $("<img>");
-        // Setting the src attribute of the image to a property pulled off the result item
-        animalImage.attr("src", results[i].images.original_still.url);
-        animalImage.attr("data-state", "still");
-        animalImage.attr("data-still", results[i].images.original_still.url);
-        animalImage.attr("data-animate", results[i].images.original.url);
-        animalImage.addClass("dynamicGIF");
-        
-        // Appending the paragraph and image tag to the animalDiv
-        animalDiv.append(p);
-        animalDiv.append(animalImage);
+         // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              // Creating a div with the class "item"
+              var gifDiv = $("<div class='item'>");
 
-        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-        $("#GIFBoard").prepend(animalDiv);
+              // Storing the result item's rating
+              var rating = results[i].rating;
 
-        $(".dynamicGIF").on("click", function() {
-          // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-          var state = $(this).attr("data-state");
-          // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-          // Then, set the image's data-state to animate
-          // Else set src to the data-still value
-          if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-          } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-          }
-        });
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + rating);
+
+              // Creating an image tag
+              var animalImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              animalImage.attr("src", results[i].images.fixed_height.url);
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              gifDiv.append(p);
+              gifDiv.append(animalImage);
+
+              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              $("#gifs-appear-here").prepend(gifDiv);
+        };
       }
 
     });
@@ -81,7 +71,7 @@ $("#submit").on("click", function() {
     var animalName = $(this).attr("data-animal");
 
     // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=4";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=5";
 
     // Performing an AJAX request with the queryURL
     $.ajax({
@@ -99,44 +89,34 @@ $("#submit").on("click", function() {
         // Looping through each result item
         for (var i = 0; i < results.length; i++) {
 
-          // Creating and storing a div tag
-          var animalDiv = $("<div>");
-          animalDiv.addClass("GIFsection");
-          
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
-          p.addClass("padding");
+        // Creating and storing a div tag
+        var animalDiv = $("<div>");
 
-          // Creating and storing an image tag
-          var animalImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-        animalImage.attr("src", results[i].images.original_still.url);
-        animalImage.attr("data-state", "still");
-        animalImage.attr("data-still", results[i].images.original_still.url);
-        animalImage.attr("data-animate", results[i].images.original.url);
-        animalImage.addClass("dynamicGIF");
-        
-        // Appending the paragraph and image tag to the animalDiv
-        animalDiv.append(p);
-        animalDiv.append(animalImage);
+         // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              // Creating a div with the class "item"
+              var gifDiv = $("<div class='item'>");
 
-        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-        $("#GIFBoard").prepend(animalDiv);
+              // Storing the result item's rating
+              var rating = results[i].rating;
 
-        $(".dynamicGIF").on("click", function() {
-          // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-          var state = $(this).attr("data-state");
-          // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-          // Then, set the image's data-state to animate
-          // Else set src to the data-still value
-          if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-          } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-          }
-        });
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + rating);
+
+              // Creating an image tag
+              var animalImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              animalImage.attr("src", results[i].images.fixed_height.url);
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              gifDiv.append(p);
+              gifDiv.append(animalImage);
+
+              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              $("#gifs-appear-here").prepend(gifDiv);
+        };
         }
       });
   });
